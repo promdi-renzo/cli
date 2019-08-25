17a559f6bfc3005c8b3247644d4d924d652c677b
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-25 12:27:36 +0800

    Add create function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-console.log(process.argv);
+import * as program from "commander";
+import * as path from "path";
+import * as shell from "shelljs";
+
+// const packageJSON = path.resolve(process.cwd(), "package.json");
+const mainJSON = path.join(__dirname, "../package.json");
+
+import(mainJSON)
+  .then(runCLI)
+  .catch(error => console.log(error));
+
+function runCLI(npm: any) {
+  program.version(npm.version).description(npm.description);
+
+  create();
+
+  program.parse(process.argv);
+}
+
+function create() {
+  program
+    .command("new <directory>")
+    .description("Create a MayaJS project")
+    .action((dir: any) => {
+      // Clone git repo for sample template
+      const sample = `git clone https://github.com/mayajs/sample.git ${dir}`;
+      if (shell.exec(sample).code !== 0) {
+        shell.echo("Error: Git clone failed");
+        shell.exit(1);
+      }
+      // Update sample templates package.json
+    });
+}
