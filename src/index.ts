d7a8478a8f52302c507108d29b461d1a74894713
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-18 20:51:31 +0800

    Remove runCLI function and import it on cli.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
 #! /usr/bin/env node
 
-import * as program from "commander";
 import * as path from "path";
-
-// TASKS
-import { createProject } from "./lib/task";
-
-interface NpmPackage {
-  version: string;
-  description: string;
-}
+import runCLI from "./lib/cli";
 
 const NPM = path.join(__dirname, "package.json");
 
 import(NPM)
   .then(runCLI)
   .catch(error => console.log(error));
-
-function runCLI(npm: NpmPackage) {
-  program
-    .version(npm.version)
-    .option("new <directory>, n <directory>", "Creates a new mayajs project based on the directory.")
-    .description(npm.description);
-
-  program
-    .command("new <directory>")
-    .alias("n")
-    .description("Create a MayaJS project")
-    .action(createProject);
-
-  program.parse(process.argv);
-}
