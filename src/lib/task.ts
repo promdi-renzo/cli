f021fcf205a78101fe5cf1bb7cd4aaf012749084
Mac Ignacio <mack_ignacio@yahoo.com>
2019-11-29 02:44:54 +0800

    Feat(task) - Create controller

--- src/lib/task.ts
+++ src/lib/task.ts
@@ -9,30 +9,23 @@ import {
   createAppModule,
   createAppRoutingModule,
   createEnvironment,
+  createController,
 } from "./create";
 
 export const createProject = (directory: string) => {
-  // ✔ Create index.ts
-  // ✔ Create README.MD
-  // ✔ Create package.json
-  // ✔ Create .gitignore
-  // ✔ Create tsconfig.json
-  // ✔ Create app.module.ts
-  // ✔ Create app.routing.module.ts
-  // ✔ Create environments
-  // create controllers
 
-  checkCurrentDirectory(directory);
+  const cur_dir = checkCurrentDirectory(directory);
 
   const tasks = new Listr([
-    { title: "Create index.ts", task: createIndex },
-    { title: "Create README.md", task: createReadMe },
-    { title: "Create package.json", task: createPackageJSON },
-    { title: "Create .gitignore", task: createGitIgnore },
-    { title: "Create tsconfig.json", task: createTsConfig },
-    { title: "Create app.module.ts", task: createAppModule },
-    { title: "Create app.routing.module.ts", task: createAppRoutingModule },
-    { title: "Create environment", task: createEnvironment },
+    { title: `Create ${cur_dir}/index.ts`, task: createIndex },
+    { title: `Create ${cur_dir}/README.md`, task: createReadMe },
+    { title: `Create ${cur_dir}/package.json`, task: createPackageJSON },
+    { title: `Create ${cur_dir}/.gitignore`, task: createGitIgnore },
+    { title: `Create ${cur_dir}/tsconfig.json`, task: createTsConfig },
+    { title: `Create ${cur_dir}/app.module.ts`, task: createAppModule },
+    { title: `Create ${cur_dir}/app.routing.module.ts`, task: createAppRoutingModule },
+    { title: `Create ${cur_dir}/src/environment`, task: createEnvironment },
+    { title: `Create ${cur_dir}/src/controller`, task: createController },
   ]);
 
   tasks.run(directory).catch((err: any) => {
