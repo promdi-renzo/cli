f5676387641f1b1164430232d9b8e29db8c2ee6b
Mac Ignacio <mack_ignacio@yahoo.com>
2019-11-28 17:28:51 +0800

    Add readme

--- src/json/index.ts
+++ src/json/index.ts
@@ -3,4 +3,17 @@ const index = {
   content: ["const server = new MayaJS(AppModule);", 'const prod = process.env.NODE_ENV === "production";', "server.prodMode(prod).start();"],
 };
 
-export { index };
+const readme = {
+  appName: "#appName",
+  description: `This project was created with [MayaJS CLI](#cli-url) version #cli-version`,
+  body: [
+    "## Development server",
+    "Run `maya serve` to run the server. Navigate to `http://localhost:3333`. Hot reload will watch for file changes and restart the server.",
+    "## Code scaffolding",
+    "Run `maya create <service|controller|model> <name|directory>` to create a new component.",
+    "## Build",
+    "Run `maya build` to build the project. Use the `--prod` flag for a production build. The build files will be created in the `dist/` directory.",
+  ],
+};
+
+export { index, readme };
