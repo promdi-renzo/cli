fb28958356902cb33342c2b722a9e60667922900
Mac Ignacio <mack_ignacio@yahoo.com>
2019-12-01 12:13:29 +0800

    Add --port flag when running shell.exec()

--- src/lib/serve.ts
+++ src/lib/serve.ts
@@ -1,9 +1,9 @@
 import * as shell from "shelljs";
 import * as chalk from "chalk";
 
-export function serve() {
-  console.log(chalk.green("** MAYA Live Development Server is on http://localhost:3333/**"));
-  const cmd = shell.exec("npx nodemon --exec npx ts-node src/index.ts");
+export function serve(port: number) {
+  console.log(chalk.green(`** MAYA Live Development Server is on http://localhost:${port}/ **\n`));
+  const cmd = shell.exec(`npx nodemon --exec npx ts-node src/index.ts --port=${port}`);
   if (cmd.code !== 0) {
     throw new Error("Error:Can't start server failed");
   }
