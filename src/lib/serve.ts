9825757810d0a7872434183c921fdbcc3ba2492e
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-17 00:46:01 +0800

    FEAT: Add detailed error message for file errors

--- src/lib/serve.ts
+++ src/lib/serve.ts
@@ -2,6 +2,8 @@ import chalk from "chalk";
 import ts from "typescript";
 import { spawn } from "child_process";
 import util from "util";
+import path from "path";
+import fs = require("fs");
 
 const exec = util.promisify(require("child_process").exec);
 
@@ -16,7 +18,13 @@ const formatHost: ts.FormatDiagnosticsHost = {
 
 function reportDiagnostic(diagnostic: ts.Diagnostic) {
   hasBuildError = true;
-  console.error("Error", diagnostic.code, ":", ts.flattenDiagnosticMessageText(diagnostic.messageText, formatHost.getNewLine()));
+  if (diagnostic.file) {
+    let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+    let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+    console.log("Error", diagnostic.code, ":", `${path.basename(diagnostic.file.fileName)} (${line + 1},${character + 1}):`, chalk.red(message));
+  } else {
+    console.error("Error", diagnostic.code, ":", ts.flattenDiagnosticMessageText(diagnostic.messageText, formatHost.getNewLine()));
+  }
 }
 
 function reportWatchStatusChanged(diagnostic: ts.Diagnostic) {
@@ -40,6 +48,15 @@ function spawnCommand(port: number) {
 }
 
 export function serve(port: number) {
+  const json = JSON.parse(fs.readFileSync("package.json", "utf8"));
+  const dependencies = json.dependencies;
+
+  const isMayaJS = Object.keys(dependencies).filter((key: string) => key.includes("@mayajs")).length > 0;
+
+  if (!isMayaJS) {
+    throw new Error(chalk.red("Couldn't find '@mayajs/core' module in this project."));
+  }
+
   const configPath = ts.findConfigFile("./", ts.sys.fileExists, "tsconfig.json");
 
   if (!configPath) {
@@ -80,7 +97,7 @@ export function serve(port: number) {
         });
     }
 
-    if (!hasLoaded) {
+    if (!hasLoaded && !hasBuildError) {
       hasLoaded = true;
       console.log(chalk.green(`\n** MAYA Live Development Server is running on`), `http://localhost:${port}/`, chalk.green("**\n"));
     }
