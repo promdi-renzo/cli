bca8b73da84405bcd38b39fdae65c60c85bbae16
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-17 02:01:03 +0800

    FEAT: Add start and finished message

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,18 +1,26 @@
 import * as shell from "shelljs";
 import ts from "typescript";
+import chalk from "chalk";
 import { errorMessage } from "./utils";
 
 export function build(options: ts.CompilerOptions): void {
+  console.log(chalk.yellow("[mayajs] Start building your project..."));
+
   let program = ts.createProgram(["src/index.ts"], options);
   let emitResult = program.emit();
   let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
 
   allDiagnostics.forEach(diagnostic => {
-    console.log(errorMessage(diagnostic));
+    console.log("\n", errorMessage(diagnostic), "\n");
   });
 
-  let exitCode = emitResult.emitSkipped ? 1 : 0;
-  process.exit(exitCode);
+  if (allDiagnostics.length > 0) {
+    console.log(chalk.green(`[mayajs] Build finished with ${allDiagnostics.length} error${allDiagnostics.length > 1 ? "s" : ""}!`));
+  } else {
+    console.log(chalk.green("[mayajs] Build finished!"));
+  }
+
+  process.exit(0);
 }
 
 export function cleanOutDir(options: { outDir: string }) {
