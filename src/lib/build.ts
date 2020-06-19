96b9b4a38b5063cc47000d99b88dcf22cb6ff080
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-19 17:47:05 +0800

    FIX: Not compiling models and services

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -2,31 +2,60 @@ import * as shell from "shelljs";
 import ts from "typescript";
 import chalk from "chalk";
 import fs from "fs";
+import path from "path";
 import { errorMessage } from "./utils";
 
-export function build(options: ts.CompilerOptions): void {
-  console.log(chalk.yellow("[mayajs] Start building your project..."));
+export function build(): void {
+  logger(`Start building your project...`);
 
-  let program = ts.createProgram(["src/index.ts"], options);
-  let emitResult = program.emit();
-  let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-  let message = "";
+  const configPath = ts.findConfigFile("./", ts.sys.fileExists, "tsconfig.json");
 
-  allDiagnostics.forEach(diagnostic => {
-    console.log("\n", errorMessage(diagnostic), "\n");
+  if (!configPath) {
+    throw new Error("Could not find a valid 'tsconfig.json'.");
+  }
+
+  const host: ts.ParseConfigFileHost = ts.sys as any;
+  const parsedCmd = ts.getParsedCommandLineOfConfigFile(configPath, {}, host);
+  const { options, fileNames } = parsedCmd as ts.ParsedCommandLine;
+
+  fileNames.map(name => {
+    logger(`Compiling ${path.basename(name)}...`);
   });
 
+  const diagnosticErrors = compile(fileNames, options);
+  let message = "";
 
-  if (allDiagnostics.length > 0) {
-    message = ` with ${allDiagnostics.length} error${allDiagnostics.length > 1 ? "s" : ""}`;
+  if (diagnosticErrors.length > 0) {
+    message = ` with ${diagnosticErrors.length} error${diagnosticErrors.length > 1 ? "s" : ""}`;
   } else {
     fs.copyFile("package.json", "dist/package.json", err => {
       if (err) throw err;
     });
   }
 
-  console.log(chalk.green(`[mayajs] Build finished${message}!`));
+  logger(`Build finished${message}!`);
   process.exit(0);
+}
+
+function compile(fileNames: string[], options: ts.CompilerOptions) {
+  const program = ts.createProgram(fileNames, options);
+  const emitResult = program.emit();
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+  let compileErrors: string[] = [];
+
+  allDiagnostics.forEach(diagnostic => {
+    const error = errorMessage(diagnostic);
+    compileErrors.push(error);
+    console.log("\n", chalk.red(error), "\n");
+  });
+
+  return compileErrors;
+}
+
+function logger(message: string) {
+  const PLUGIN_NAME = chalk.yellow(`[mayajs]`);
+  console.log(PLUGIN_NAME, chalk.green(message));
 }
 
 export function cleanOutDir(options: { outDir: string }) {
