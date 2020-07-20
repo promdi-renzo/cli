1b76323ef3337e5c64f4f9fef4991556c73a1cc7
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-20 23:08:35 +0800

    FIX: Filename of errored files

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,15 @@ export function errorMessage(diagnostic: ts.Diagnostic): string {
   if (diagnostic.file) {
     let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
     let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+    let fileName = diagnostic.file.fileName;
     const code = chalk.yellow("TS" + diagnostic.code);
-    const fileName = chalk.green(path.basename(diagnostic.file.fileName));
-    return `Error : ${code} ${fileName} (LINE=${chalk.yellow(line + 1)},CHAR=${chalk.yellow(character + 1)}): ${chalk.red(message)}`;
+
+    if (diagnostic.file.fileName.includes("src")) {
+      const pathName = diagnostic.file.fileName.match(/([\/|\\]src.*)/) || [];
+      fileName = `.${pathName[0]}`;
+    }
+
+    return `Error : ${code} ${chalk.green(fileName)} (LINE=${chalk.yellow(line + 1)},CHAR=${chalk.yellow(character + 1)}): ${chalk.red(message)}`;
   } else {
     return ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
   }
