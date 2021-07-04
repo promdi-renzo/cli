1a404287bab6483e7e08602ef6a32e33af0ad522
Mac Ignacio <mack_ignacio@yahoo.com>
2021-07-04 17:31:18 +0800

    FEAT: Add updateTemplateList function

--- src/lib/template.ts
+++ src/lib/template.ts
@@ -1,4 +1,8 @@
+import { getContentsUTF8FromDirname } from "./utils";
 import * as shell from "shelljs";
+import https from "https";
+import path from "path";
+import fs from "fs";
 
 const updateTemplateFolder = async (ctx: any, task: any) => {
   const temp = `${ctx.templatesFolderDir}/temp`;
@@ -13,4 +17,25 @@ const cloneTemplateRepo = async (ctx: any, task: any) => {
   shell.rm("-rf", [`${ctx.templatesFolderDir}/.git`, `${ctx.templatesFolderDir}/README.md`]);
 };
 
-export { updateTemplateFolder, cloneTemplateRepo };
+const updateTemplateList = async (ctx: any, task: any) => {
+  const promise = new Promise((resolve, reject) => {
+    https
+      .get("https://raw.githubusercontent.com/mayajs/templates/master/templates.json", (res) => {
+        let data: any[] = [];
+        res.on("data", (chunk) => data.push(chunk));
+        res.on("end", () => {
+          const templateData = JSON.parse(Buffer.concat(data).toString());
+          resolve(JSON.stringify(templateData, null, 2));
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+  const templateJSON = path.resolve(ctx.templatesFolderDir, "./templates.json");
+  fs.writeFileSync(`${ctx.templatesFolderDir}/templates.json`, await promise);
+  ctx["DATA_JSON"] = getContentsUTF8FromDirname(templateJSON);
+  ctx["promise"] = promise;
+};
+
+export { updateTemplateFolder, cloneTemplateRepo, updateTemplateList };
